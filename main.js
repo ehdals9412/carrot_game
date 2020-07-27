886ab79d8e208a835152d465f7efa60f01efb5ca
dongmin <ehdals9412@gmail.com>
2020-07-27 13:56:09 +0900

    css, image random print!

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
 'use strict';
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const CARROT_SIZE = 80;
 
-const display = document.querySelector('.game__display');
-const playBtn = document.querySelector('.game__play-btn');
+const gameBtn = document.querySelector('.game__button');
 const timer = document.querySelector('.game__timer');
+const score = document.querySelector('.game__score');
 
-// play button click event!
-playBtn.addEventListener('click', () => {
-  console.log('play!');
-  timeCheck();
+gameBtn.addEventListener('click', () => {
+  removeHide();
+  initGame();
 });
 
-// 1. timer function!
-function timeCheck() {
-  let time = 10;
-  const timeCheck = setInterval(() => {
-    time -= 1;
-    console.log(time);
-    if (time == 0) {
-      alert('time over!');
-      clearInterval(timeCheck);
-    }
-  }, 1000);
+function removeHide() {
+  timer.classList.remove('hide');
+  score.classList.remove('hide');
 }
 
-// 2. bug & carror result function
-function createBug() {
-  // const bug = document.createElement('img');
-  // bug.setAttribute('src', 'img/bug.png');
-  // bug.setAttribute('class', 'bug');
-  // const carrot = document.createElement('img');
-  // carrot.setAttribute('src', 'img/carrot.png');
-  // carrot.setAttribute('class', 'carrot');
-  // carrot.setAttribute('position', 'relative');
-  // carrot.setAttribute('top', `${350}px`);
-  // carrot.setAttribute('left', `${400}px`);
-  // section1.appendChild(bug);
-  // section1.appendChild(carrot);
+// 필드에 아이템 추가
+function initGame() {
+  addItem('carrot', 5, 'img/carrot.png');
+  addItem('bug', 5, 'img/bug.png');
+}
+
+// 아이템 생성 함수
+function addItem(className, count, imgPath) {
+  const x1 = 0;
+  const y1 = 0;
+  const x2 = fieldRect.width - CARROT_SIZE;
+  const y2 = fieldRect.height - CARROT_SIZE;
+
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('img');
+    item.setAttribute('class', className);
+    item.setAttribute('src', imgPath);
+    item.style.position = 'absolute';
+    const x = randomNumber(x1, x2);
+    const y = randomNumber(y1, y2);
+    item.style.left = `${x}px`;
+    item.style.top = `${y}px`;
+    field.appendChild(item);
+  }
+}
+
+// 랜덤 좌표 출력
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
